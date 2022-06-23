79ede15a02935bafcb12f7083f3ce44203f63b23
Opetimistic <akinwandeopeoluwa@yahoo.com>
2022-06-23 16:06:46 +0100

    Personal Info newly committed file

---
+++ info.js
@@ -0,0 +1,17 @@
+// function myName() {
+//     return document.getElementById("name").innerHTML = "My name is Opeoluwa!";
+// }
+
+const fullName = "OPEOLUWA AKINWANDE";
+const height = " 5'4.2\"\ "
+const country = " NIGERIA"
+
+window.alert("\n Name: " + fullName + "\n Height: " + height + "\n Country: " + country);
+
+
+// console.log("\n NAME: " + fullName + "\n HEIGHT: " + height + "\n COUNTRY: " + country);
+
+
+
+
+
